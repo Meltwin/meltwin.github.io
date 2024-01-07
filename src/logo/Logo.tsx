b8c33199055916d1c8e2a10554d2a17b7c7c0a09
G Côte <meltwin158@gmail.com>
2024-01-07 23:22:08 +0100

    Responsive design

--- src/logo/Logo.tsx
+++ src/logo/Logo.tsx
@@ -4,6 +4,6 @@ export function Logo() {
     return (<div id="logo">
         <img src="https://github.com/meltwin.png"></img>
         <h1>Meltwin</h1>
-        <h2>Robotic Engineering Student</h2>
+        <h2><span className="italic">Robotic Engineering Student</span><br />🇫🇷 Centrale Nantes | DTU 🇩🇰</h2>
     </div>)
 }
