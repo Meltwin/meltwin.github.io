fe2532dd925e1c18a2738102c8b607c8d091398e
G Côte <meltwin158@gmail.com>
2024-01-08 09:22:46 +0100

    Background & Logo Tweaking

--- src/logo/Logo.tsx
+++ src/logo/Logo.tsx
@@ -2,7 +2,7 @@ import "./logo.scss"
 
 export function Logo() {
     return (<div id="logo">
-        <img src="https://github.com/meltwin.png"></img>
+        <a href="https://github.com/Meltwin"><img src="https://github.com/meltwin.png" /></a>
         <h1>Meltwin</h1>
         <h2><span className="italic">Robotic Engineering Student</span><br />🇫🇷 Centrale Nantes | DTU 🇩🇰</h2>
     </div>)
